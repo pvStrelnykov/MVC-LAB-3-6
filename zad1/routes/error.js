9bd0cd90044449b5a3463519dac9631fceba41b1
StrelnikovPavlo <strelnikovpavel82@gmail.com>
2024-04-23 17:19:29 +0200

    zadanie 1

---
+++ zad1/routes/error.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const errorController = require('../controllers/error')
+const router = express.Router()
+
+router.use(errorController.getNotFoundPage)
+
+module.exports = router
